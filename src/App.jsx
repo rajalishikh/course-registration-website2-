66fb5be9f7297f10ab9226d4e975a4332f54d3b5
raj ali sheikhe <nahidujjamanraj@gmail.comm>
2024-02-25 08:49:31 +0600

    add my header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Header from './Component/Header/Header'
 
 function App() {
   
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
      
-    
+    <Header></Header>
       
       
     </>
---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-center m-8">Course Registration </h1>
+            
+        </div>
+    );
+};
+
+export default Header;
